37a20fadd8cec864c630e9538a2079eac3b7c03d
VideoJames13 <JamesMehran1@gmail.com>
2025-09-16 00:07:43 -0400

    basic functionality, accepts input via command line when you call the program

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+import sys
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    if len(sys.argv) < 2:
+        print("Error: no input")
+        sys.exit(1)
+
+    else:
+        prompt = sys.argv[1]
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', contents=prompt
+    )
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        print("Response:")
+        print(response.text)
+        
+
+
+if __name__ == "__main__":
+    main()
